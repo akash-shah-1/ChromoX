71bccf59942f63dbe035e5db0b201f217fb20823
Akash-shah-cis <akash.s@cisinlabs.com>
2024-02-21 18:57:04 +0530

    cors

--- client/src/pages/products/ProductList/ProductList.js
+++ client/src/pages/products/ProductList/ProductList.js
@@ -15,7 +15,7 @@ const ProductList = ({filter,sort}) => {
     const fetchProducts = async () => {
       try {
         // Construct the API request URL with filter and sort options
-        let url = 'http://localhost:8080/product?';
+        let url = 'https://chromo-x-test.vercel.app/product?';
         if (filter) {
           for (const key in filter) {
             if (filter.hasOwnProperty(key)) {
--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 
+// Add CORS headers middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the HTTP methods you need
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow the headers you need
+    next();
+});
+
 app.use(require('./routes/v1/index'))
 
 
